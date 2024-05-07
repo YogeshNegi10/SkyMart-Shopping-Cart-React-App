ea18e805df1e19e21a0f3a76a2509b4af7b5d7bf
Yogesh Negi <yogeshlm13@gmail.com>
2024-05-07 20:05:57 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
   },[])
 
   return (
-    <header className=" mt-6 inset-x-0 top-0 z-30 mx-auto w-full max-w-screen-sm border border-gray-100 bg-white/80 py-3 shadow backdrop-blur-lg md:top-6 md:rounded-3xl lg:max-w-screen-lg ">
+    <header className=" mt-6 inset-x-0 top-0  mx-auto w-full max-w-screen-sm border border-gray-100 bg-white/80 py-3 shadow backdrop-blur-lg md:top-6 md:rounded-3xl lg:max-w-screen-lg ">
       <div className="px-4">
         <div className="flex items-center justify-between">
           <div className="flex shrink-0 justify-center items-center gap-2 cursor-pointer">
